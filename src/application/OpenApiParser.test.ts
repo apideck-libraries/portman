8a212d653e798083dba6bcad40ffb9810309a355
nick_lloyd <nick@nicklloyd.co>
2021-05-31 23:36:17 +0200

    add safenav in tests

--- src/application/OpenApiParser.test.ts
+++ src/application/OpenApiParser.test.ts
@@ -24,8 +24,8 @@ describe('OpenApiParser', () => {
   describe('getOperationById', () => {
     it('should be able to retrieve a MappedOperation by operationId', async () => {
       const operation = parser.getOperationById('usersAll')
-      expect(operation.id).toStrictEqual('usersAll')
-      expect(operation.schema).toMatchSnapshot()
+      expect(operation?.id).toStrictEqual('usersAll')
+      expect(operation?.schema).toMatchSnapshot()
     })
 
     it('should be fail gracefully if not found', async () => {
@@ -38,13 +38,13 @@ describe('OpenApiParser', () => {
     it('should be able to retrieve a read MappedOperation by operationId', async () => {
       const operation = parser.getOperationByPath('GET::/crm/companies/{id}')
       expect(operation?.id).toStrictEqual('companiesOne')
-      expect(operation.schema).toMatchSnapshot()
+      expect(operation?.schema).toMatchSnapshot()
     })
 
     it('should be able to retrieve a MappedOperation by operationId with path param', async () => {
       const operation = parser.getOperationByPath('POST::/crm/companies')
       expect(operation?.id).toStrictEqual('companiesAdd')
-      expect(operation.schema).toMatchSnapshot()
+      expect(operation?.schema).toMatchSnapshot()
     })
 
     it('should be fail gracefully if not found', async () => {
