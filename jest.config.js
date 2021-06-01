9e882b75eda75189ab7f669262037e72a398549b
nick_lloyd <nick@nicklloyd.co>
2021-06-01 17:28:27 +0200

    pull coverage thresholds for now

--- jest.config.js
+++ jest.config.js
@@ -4,13 +4,5 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest'
-  },
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 95,
-      lines: 95,
-      statements: 95
-    }
   }
 }
