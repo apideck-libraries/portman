03b1ea40f20b94adc6a55b29f8b0599634bda2ee
nick_lloyd <nick@nicklloyd.co>
2021-06-03 15:34:15 +0100

    update snaps

--- src/application/PostmanParser.ts
+++ src/application/PostmanParser.ts
@@ -30,7 +30,7 @@ export class PostmanParser implements IPostmanParser {
     this.oasParser = oasParser
 
     if (postmanObj) {
-      this.collection = new Collection(postmanObj)
+      this.collection = postmanObj
     } else if (inputFile) {
       const postmanJson = path.resolve(inputFile)
       this.collection = new Collection(JSON.parse(fs.readFileSync(postmanJson).toString()))
