eab90571313d67c62fdd917bd2f46034b33817a6
Thim <timhaselaars@gmail.com>
2021-06-21 10:54:29 +0200

    Added unit tests for the variations with "openApiResponse" target

--- src/application/TestSuite.test.ts
+++ src/application/TestSuite.test.ts
@@ -9,23 +9,34 @@ describe('TestSuite', () => {
   let postmanParser: PostmanParser
   let oasParser: OpenApiParser
   let testSuiteService: TestSuite
+  let testSuiteServiceForVariation: TestSuite
 
   const postmanJson = '__tests__/fixtures/crm.postman.json'
   const oasYml = '__tests__/fixtures/crm.yml'
   const postmanConfigFile = '__tests__/fixtures/portman.crm.json'
+  const portmanVariationConfigFile = '__tests__/fixtures/portman-variations.crm.json'
 
   beforeEach(async () => {
     oasParser = new OpenApiParser()
     await oasParser.convert({ inputFile: oasYml })
     const postmanObj = JSON.parse(fs.readFileSync(postmanJson).toString())
-    const config = await getConfig(postmanConfigFile)
+    let config = await getConfig(postmanConfigFile)
+
     postmanParser = new PostmanParser({ postmanObj: postmanObj, oasParser: oasParser })
 
     testSuiteService = new TestSuite({ oasParser, postmanParser, config })
+
+    config = await getConfig(portmanVariationConfigFile)
+    testSuiteServiceForVariation = new TestSuite({ oasParser, postmanParser, config })
   })
 
   it('should generateContractTests', () => {
     testSuiteService.generateContractTests()
+    expect(omitKeys(testSuiteService.collection.toJSON(), ['id', '_postman_id'])).toMatchSnapshot()
+  })
+
+  it('should generateContractTests for variations', () => {
+    testSuiteServiceForVariation.generateContractTests()
     expect(omitKeys(testSuiteService.collection.toJSON(), ['id', '_postman_id'])).toMatchSnapshot()
   })
 
