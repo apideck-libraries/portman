ee70b23d44ef9f7172373c70384d1078287bcf3b
Thim <timhaselaars@gmail.com>
2021-07-07 21:24:42 +0200

    Add tests linked to #75

--- src/application/globals/writeRawReplacements.test.ts
+++ src/application/globals/writeRawReplacements.test.ts
@@ -15,4 +15,31 @@ describe('writeRawReplacements', () => {
       '{"root": "bars", "nested": {"bars": "monkey"}, "arr": ["monkey", "bars"]}'
     )
   })
+
+  it('should use config :: to replace all found values in string', () => {
+    const config = [
+      {
+        searchFor: '::',
+        replaceWith: ':-:'
+      }
+    ]
+
+    const string = '{"exec": ["[GET]::/ - Status code is 2xx"]}'
+    const result = writeRawReplacements(string, config)
+    expect(result).toStrictEqual('{"exec": ["[GET]:-:/ - Status code is 2xx"]}')
+  })
+
+  it('should use config :: to replace all found values in escaped string', () => {
+    const config = [
+      {
+        searchFor: '::',
+        replaceWith: ':-:'
+      }
+    ]
+
+    const string = '{"exec": ["// Validate status 2xx \npm.test("[GET]::/portal/"]}'
+    const result = writeRawReplacements(string, config)
+    console.log('result', result)
+    expect(result).toStrictEqual('{"exec": ["// Validate status 2xx \npm.test("[GET]:-:/portal/"]}')
+  })
 })
