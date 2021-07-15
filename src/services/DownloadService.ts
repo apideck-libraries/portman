53574a3cd6b771ea5c5b6804ef68e060ef5862c7
Thim <timhaselaars@gmail.com>
2021-07-15 16:25:58 +0200

    Enhancement - Phased out "node-fetch" in favour of "Axios" + improved error handling

--- src/services/DownloadService.ts
+++ src/services/DownloadService.ts
@@ -3,23 +3,29 @@ import axios from 'axios'
 
 export class DownloadService {
   async get(url: string): Promise<string> {
-    const res = await axios({
-      method: 'get',
-      url: url,
-      responseType: 'stream'
-    })
-    const fileName = url.replace(/\/$/, '').split('/').pop()
-    const filePath = `./tmp/${fileName}`
+    try {
+      const res = await axios({
+        method: 'get',
+        url: url,
+        responseType: 'stream'
+      })
+      const fileName = url.replace(/\/$/, '').split('/').pop()
+      const filePath = `./tmp/${fileName}`
 
-    return await new Promise((resolve, reject) => {
-      const fileStream = createWriteStream(filePath)
-      res.data.pipe(fileStream)
-      res.data.on('error', err => {
-        reject(err.toString())
+      return await new Promise((resolve, reject) => {
+        const fileStream = createWriteStream(filePath)
+        res.data.pipe(fileStream)
+        res.data.on('error', err => {
+          reject(err.toString())
+        })
+        fileStream.on('finish', () => {
+          resolve(filePath)
+        })
       })
-      fileStream.on('finish', () => {
-        resolve(filePath)
-      })
-    })
+    } catch (axiosError) {
+      console.error('\x1b[31m', `OAS URL error - There is an problem with the url: "${url}"`)
+      console.error('\x1b[31m', axiosError.message)
+      process.exit(0)
+    }
   }
 }
