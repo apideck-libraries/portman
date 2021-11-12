e6ee043c02c59ac4e24fc16b4c54b942c6a795e7
Thim <timhaselaars@gmail.com>
2021-11-12 11:32:03 +0100

    Extended tests for assignVarFromRequestBody

--- src/application/variables/assignVarFromRequestBody.test.ts
+++ src/application/variables/assignVarFromRequestBody.test.ts
@@ -13,13 +13,42 @@ describe('assignVarFromRequestBody', () => {
     jest.clearAllMocks()
   })
 
-  it.skip('should add postman collection var with name and request body value', async () => {
+  it('should add postman collection var with name and request body value', async () => {
     const varSetting = {
-      requestBodyProp: 'company_name',
-      name: 'leadsAdd.company_name'
+      requestBodyProp: 'name',
+      name: 'leadsAdd.name'
     }
 
-    // TODO
+    pmOperation = assignVarFromRequestBody(varSetting, pmOperation)
+    const pmTest = pmOperation.getTests()
+    expect(pmTest.script.exec).toMatchSnapshot()
+  })
+
+  it('should add postman collection var without name and request body value', async () => {
+    const varSetting = {
+      requestBodyProp: 'name'
+    }
+
+    pmOperation = assignVarFromRequestBody(varSetting, pmOperation)
+    const pmTest = pmOperation.getTests()
+    expect(pmTest.script.exec).toMatchSnapshot()
+  })
+
+  it('should add postman collection var without name and request body array value', async () => {
+    const varSetting = {
+      requestBodyProp: 'websites[0].url'
+    }
+
+    pmOperation = assignVarFromRequestBody(varSetting, pmOperation)
+    const pmTest = pmOperation.getTests()
+    expect(pmTest.script.exec).toMatchSnapshot()
+  })
+
+  it('should not fail when on unexisting request body value', async () => {
+    const varSetting = {
+      requestBodyProp: 'foo'
+    }
+
     pmOperation = assignVarFromRequestBody(varSetting, pmOperation)
     const pmTest = pmOperation.getTests()
     expect(pmTest.script.exec).toMatchSnapshot()
