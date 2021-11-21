3986e917fa0b5542f82b812c7d919985d11afb9c
Thim <timhaselaars@gmail.com>
2021-11-21 14:21:14 +0100

    Updated the Postman API error response message

--- src/services/PostmanApiService.ts
+++ src/services/PostmanApiService.ts
@@ -260,7 +260,7 @@ export class PostmanApiService {
         spinner.succeed(`Upload to Postman Succeeded`)
       } else {
         spinner.succeed(
-          `Upload to Postman completed with status: ${responseStatusCode}. \n\n Please review your collection within Postman as they can respond with a 504 timeout but still import your collection`
+          `Upload to Postman completed with status: ${responseStatusCode}. \n\n Please review your collection within Postman as they can respond with a 5xx but still import your collection`
         )
       }
       return JSON.stringify({ status: 'success', data: { ...respData, collection } }, null, 2)
