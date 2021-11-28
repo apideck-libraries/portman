d528eb63988daced00ec4a45a93d2f85c7a970ea
Thim <timhaselaars@gmail.com>
2021-11-28 15:21:17 +0100

    Refactored renderChain function tests to support node 12 logic

--- src/utils/renderChainPath.test.ts
+++ src/utils/renderChainPath.test.ts
@@ -1,38 +1,61 @@
 import { renderChainPath } from './renderChainPath'
 
 describe('renderChainPath', () => {
+  const m = process.version.match(/(\d+)\.(\d+)\.(\d+)/) || ['10.0.0', '10']
+  const [major] = m.slice(1).map(_ => parseInt(_))
+
   it('should render chained dot notation', () => {
     const path = 'foo.bar'
 
     const result = renderChainPath(path)
-    expect(result).toEqual('foo?.bar')
+    if (major < 14) {
+      expect(result).toEqual('foo.bar')
+    } else {
+      expect(result).toEqual('foo?.bar')
+    }
   })
 
   it('should render multiple chained dot notation', () => {
     const path = 'foo.bar.marco.polo'
 
     const result = renderChainPath(path)
-    expect(result).toEqual('foo?.bar?.marco?.polo')
+    if (major < 14) {
+      expect(result).toEqual('foo.bar.marco.polo')
+    } else {
+      expect(result).toEqual('foo?.bar?.marco?.polo')
+    }
   })
 
   it('should render chained array notation', () => {
     const path = 'foo[0]'
 
     const result = renderChainPath(path)
-    expect(result).toEqual('foo?.[0]')
+    if (major < 14) {
+      expect(result).toEqual('foo[0]')
+    } else {
+      expect(result).toEqual('foo?.[0]')
+    }
   })
 
   it('should render multiple chained array notation', () => {
     const path = 'foo[0].bar[1].marco[2].polo[3]'
 
     const result = renderChainPath(path)
-    expect(result).toEqual('foo?.[0]?.bar?.[1]?.marco?.[2]?.polo?.[3]')
+    if (major < 14) {
+      expect(result).toEqual('foo[0].bar[1].marco[2].polo[3]')
+    } else {
+      expect(result).toEqual('foo?.[0]?.bar?.[1]?.marco?.[2]?.polo?.[3]')
+    }
   })
 
   it('should render chained array & dot notation', () => {
     const path = 'foo[0].bar'
 
     const result = renderChainPath(path)
-    expect(result).toEqual('foo?.[0]?.bar')
+    if (major < 14) {
+      expect(result).toEqual('foo[0].bar')
+    } else {
+      expect(result).toEqual('foo?.[0]?.bar')
+    }
   })
 })
