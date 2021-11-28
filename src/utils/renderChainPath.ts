db6dfc2da4134154df258808de8c2ef55ea29cc3
Thim <timhaselaars@gmail.com>
2021-11-28 15:10:34 +0100

    Refactored renderChain function to skip optional chaining for Node 12 or lower

--- src/utils/renderChainPath.ts
+++ src/utils/renderChainPath.ts
@@ -1,8 +1,15 @@
 /**
- * Method to render a safe, chained path notation
+ * Method to render a safe, optional chained path notation
  * @param path the path definition (example: website[0].url)
  */
 export const renderChainPath = (path: string): string => {
+  const m = process.version.match(/(\d+)\.(\d+)\.(\d+)/)
+  const [major] = m.slice(1).map(_ => parseInt(_))
+  if (major < 14) {
+    // Optional chaining is only supported from Node version 14, for lower versions we will return the less safe path
+    return path
+  }
+  // Transform the path to optional chained syntax
   // eslint-disable-next-line no-useless-escape
   return path.replace(/\./g, '?.').replace(/[\[']+/g, '?.[')
 }
