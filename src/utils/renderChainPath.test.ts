636c34e06e63beb2a62e5b9e0cb8445f19c1ab11
thim81 <timhaselaars@gmail.com>
2021-11-29 09:39:03 +0100

    Support Node 12 (#175)
    * Set minimum node version to 12
    
    * Bumped openapi-format to support node 12
    
    * Refactored renderChain function tests to support node 12 logic
    
    * Refactored renderChain function to render the full IF
    
    * Implemented tests for the "renderChainPath" function to support node 12 logic/legacy mode

--- src/utils/renderChainPath.test.ts
+++ src/utils/renderChainPath.test.ts
@@ -1,11 +1,32 @@
 import { renderChainPath } from './renderChainPath'
 
 describe('renderChainPath', () => {
+  it('should return untouched property', () => {
+    const path = 'foo'
+
+    const result = renderChainPath(path)
+    expect(result).toEqual('foo')
+  })
+
+  it('should return untouched property, legacy mode', () => {
+    const path = 'foo'
+
+    const result = renderChainPath(path, true)
+    expect(result).toEqual('foo')
+  })
+
   it('should render chained dot notation', () => {
     const path = 'foo.bar'
 
     const result = renderChainPath(path)
     expect(result).toEqual('foo?.bar')
+  })
+
+  it('should render chained dot notation, legacy mode', () => {
+    const path = 'foo.bar'
+
+    const result = renderChainPath(path, true)
+    expect(result).toEqual('foo && foo.bar')
   })
 
   it('should render multiple chained dot notation', () => {
@@ -15,11 +36,25 @@ describe('renderChainPath', () => {
     expect(result).toEqual('foo?.bar?.marco?.polo')
   })
 
+  it('should render multiple chained dot notation, legacy mode', () => {
+    const path = 'foo.bar.marco.polo'
+
+    const result = renderChainPath(path, true)
+    expect(result).toEqual('foo && foo.bar && foo.bar.marco && foo.bar.marco.polo')
+  })
+
   it('should render chained array notation', () => {
     const path = 'foo[0]'
 
     const result = renderChainPath(path)
     expect(result).toEqual('foo?.[0]')
+  })
+
+  it('should render chained array notation, legacy mode', () => {
+    const path = 'foo[0]'
+
+    const result = renderChainPath(path, true)
+    expect(result).toEqual('foo[0]')
   })
 
   it('should render multiple chained array notation', () => {
@@ -29,10 +64,26 @@ describe('renderChainPath', () => {
     expect(result).toEqual('foo?.[0]?.bar?.[1]?.marco?.[2]?.polo?.[3]')
   })
 
+  it('should render multiple chained array notation, legacy mode', () => {
+    const path = 'foo[0].bar[1].marco[2].polo[3]'
+
+    const result = renderChainPath(path, true)
+    expect(result).toEqual(
+      'foo[0] && foo[0].bar[1] && foo[0].bar[1].marco[2] && foo[0].bar[1].marco[2].polo[3]'
+    )
+  })
+
   it('should render chained array & dot notation', () => {
     const path = 'foo[0].bar'
 
     const result = renderChainPath(path)
     expect(result).toEqual('foo?.[0]?.bar')
+  })
+
+  it('should render chained array & dot notation, legacy mode', () => {
+    const path = 'foo[0].bar'
+
+    const result = renderChainPath(path, true)
+    expect(result).toEqual('foo[0] && foo[0].bar')
   })
 })
