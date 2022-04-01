7db65f0cdcec54829400d98ad86362e4015ffc33
Thim <timhaselaars@gmail.com>
2022-04-01 09:00:09 +0200

    Extended support for the OpenAPI methods

--- src/utils/methods.ts
+++ src/utils/methods.ts
@@ -1,1 +1,2 @@
-export const METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+// https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#pathItemObject
+export const METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD', 'TRACE']
