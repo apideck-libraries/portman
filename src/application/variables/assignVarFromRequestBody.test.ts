971f3ba877fa9271ebac27854e20b175f0f6a567
Thim <timhaselaars@gmail.com>
2022-06-06 14:53:10 +0200

    Added additional tests for assign variables

--- src/application/variables/assignVarFromRequestBody.test.ts
+++ src/application/variables/assignVarFromRequestBody.test.ts
@@ -1,12 +1,17 @@
-import { getPostmanMappedCreateOperation } from '../../../__tests__/testUtils/getPostmanMappedOperation'
+import {
+  getPostmanMappedCreateArrayOperation,
+  getPostmanMappedCreateOperation
+} from '../../../__tests__/testUtils/getPostmanMappedOperation'
 import { assignVarFromRequestBody } from '../../application'
 import { PostmanMappedOperation } from '../../postman'
 
 describe('assignVarFromRequestBody', () => {
   let pmOperation: PostmanMappedOperation
+  let pmArrayOperation: PostmanMappedOperation
 
   beforeEach(async () => {
     pmOperation = await getPostmanMappedCreateOperation()
+    pmArrayOperation = await getPostmanMappedCreateArrayOperation()
   })
 
   afterEach(() => {
@@ -73,6 +78,84 @@ describe('assignVarFromRequestBody', () => {
 
     pmOperation = assignVarFromRequestBody(varSetting, pmOperation, { logAssignVariables: false })
     const pmTest = pmOperation.getTests()
+    expect(pmTest.script.exec).toMatchSnapshot()
+  })
+
+  it.skip('should add postman collection var with name and full request body value from a root element', async () => {
+    const varSetting = {
+      requestBodyProp: '.',
+      name: 'leadsAdd.name'
+    }
+
+    pmArrayOperation = assignVarFromRequestBody(varSetting, pmArrayOperation)
+    const pmTest = pmArrayOperation.getTests()
+    expect(pmTest.script.exec).toMatchSnapshot()
+  })
+
+  it('should add postman collection var with name and request body value from a root element', async () => {
+    const varSetting = {
+      requestBodyProp: '[0].name',
+      name: 'leadsAdd.name'
+    }
+
+    pmArrayOperation = assignVarFromRequestBody(varSetting, pmArrayOperation)
+    const pmTest = pmArrayOperation.getTests()
+    expect(pmTest.script.exec).toMatchSnapshot()
+  })
+
+  it('should add postman collection var without name and request body value from a root element', async () => {
+    const varSetting = {
+      requestBodyProp: '[0].name'
+    }
+
+    pmArrayOperation = assignVarFromRequestBody(varSetting, pmArrayOperation)
+    const pmTest = pmArrayOperation.getTests()
+    expect(pmTest.script.exec).toMatchSnapshot()
+  })
+
+  it('should add postman collection var without name and request body array value from a root element', async () => {
+    const varSetting = {
+      requestBodyProp: '[0].websites[0].url'
+    }
+
+    pmArrayOperation = assignVarFromRequestBody(varSetting, pmArrayOperation)
+    const pmTest = pmArrayOperation.getTests()
+    expect(pmTest.script.exec).toMatchSnapshot()
+  })
+
+  it('should not fail when on unexisting request body value from a root element', async () => {
+    const varSetting = {
+      requestBodyProp: '[0].foo'
+    }
+
+    pmArrayOperation = assignVarFromRequestBody(varSetting, pmArrayOperation)
+    const pmTest = pmArrayOperation.getTests()
+    expect(pmTest.script.exec).toMatchSnapshot()
+  })
+
+  it('should show the log output for request body assign variable from a root element', async () => {
+    const varSetting = {
+      requestBodyProp: '[0].name',
+      name: 'leadsAdd.name'
+    }
+
+    pmArrayOperation = assignVarFromRequestBody(varSetting, pmArrayOperation, {
+      logAssignVariables: true
+    })
+    const pmTest = pmArrayOperation.getTests()
+    expect(pmTest.script.exec).toMatchSnapshot()
+  })
+
+  it('should not show the log output for request body assign variable from a root element', async () => {
+    const varSetting = {
+      requestBodyProp: '[0].name',
+      name: 'leadsAdd.name'
+    }
+
+    pmArrayOperation = assignVarFromRequestBody(varSetting, pmArrayOperation, {
+      logAssignVariables: false
+    })
+    const pmTest = pmArrayOperation.getTests()
     expect(pmTest.script.exec).toMatchSnapshot()
   })
 })
