092e7ff4c10e07ebd07e1ef71bd7a6a46a18aa59
Thim <timhaselaars@gmail.com>
2024-09-28 19:46:25 +0200

    Warn for missing openApiOperationId

--- src/lib/clearTmpDirectory.ts
+++ src/lib/clearTmpDirectory.ts
@@ -5,8 +5,6 @@ export const clearTmpDirectory = async (): Promise<Option.Option<string>> => {
   try {
     await fs.emptyDir(`${__dirname}/../../tmp/working`)
     await fs.rmdir(`${__dirname}/../../tmp/working`)
-    // await fs.emptyDir(`${__dirname}/../../tmp/converted`)
-    // await fs.rmdir(`${__dirname}/../../tmp/converted`)
   } catch (error) {
     return Option.some(error.toString())
   }
