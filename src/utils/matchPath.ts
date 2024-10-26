1343877d8a1f793db7f9b09cedea1c884efb960b
Thim <timhaselaars@gmail.com>
2024-10-26 10:25:20 +0200

    matchPath - Improved matching (#669)

--- src/utils/matchPath.ts
+++ src/utils/matchPath.ts
@@ -1,12 +1,45 @@
 import { pathToRegExp } from './pathToRegex'
 
 export const matchPath = (targetPath: string | RegExp, operationPath: string): boolean => {
+  // If targetPath is already a regular expression, use it
   const expression = targetPath instanceof RegExp ? targetPath : pathToRegExp(targetPath)
   const match = expression.exec(operationPath) || false
-  // Matches in strict mode: match string should equal to input (url)
-  // Otherwise loose matches will be considered truthy:
-  // match('/messages/:id', '/messages/123/users') // true
-  // eslint-disable-next-line one-var,no-implicit-coercion
+
+  // If there's no match, return false
+  if (!match) return false
+
+  // Split both targetPath and operationPath into segments
+  const targetSegments = typeof targetPath === 'string' ? targetPath.split('/') : []
+  const operationSegments = operationPath.split('/')
+
+  // Ensure the segment lengths match when there is no wildcard in targetPath
+  if (targetSegments.length > 0 && operationSegments.length > 0) {
+    const lastTargetSegment = targetSegments[targetSegments.length - 1]
+    const lastOperationSegment = operationSegments[operationSegments.length - 1]
+
+    // If the last segment of targetPath is a wildcard (*), we allow any last segment
+    if (lastTargetSegment === '*') {
+      return true
+    }
+
+    // If the lengths of the paths don't match and there is no wildcard, return false
+    if (targetSegments.length !== operationSegments.length) {
+      return false
+    }
+
+    // If the last segment is a parameter (e.g., :id), allow any last segment
+    if (lastTargetSegment.startsWith(':')) {
+      return true
+    }
+
+    // Ensure the last segments match exactly if it's not a wildcard or parameter
+    if (lastTargetSegment !== lastOperationSegment) {
+      return false
+    }
+  }
+
+  // Strict match check: the entire matched string should equal the input string
   const matches = targetPath instanceof RegExp ? !!match : !!match && match[0] === match.input
+
   return matches
 }
