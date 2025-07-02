2fa6bea42909182190399883be4d24b9c538aedd
Thim <timhaselaars@gmail.com>
2025-07-02 08:42:44 +0200

    feat: Handle multiple Content-Types

--- src/application/TestSuiteVariations.test.ts
+++ src/application/TestSuiteVariations.test.ts
@@ -12,7 +12,7 @@ describe('TestSuite Variations', () => {
   let oasParser: OpenApiParser
   let testSuite: TestSuite
 
-  const postmanJson = '__tests__/fixtures/crm.postman.json'
+  const postmanJson = '__tests__/fixtures/crm-multi-responses.postman.json'
   const oasYml = '__tests__/fixtures/crm.yml'
   const portmanConfigFile = '__tests__/fixtures/portman-variations.crm.json'
 
@@ -43,6 +43,60 @@ describe('TestSuite Variations', () => {
   })
 
   it('should generateVariationTests for variations', () => {
+    testSuite.generateVariationTests()
+    expect(omitKeys(testSuite.collection.toJSON(), ['id', '_postman_id'])).toMatchSnapshot()
+  })
+})
+
+describe('TestSuite Variations openApiResponse content-types', () => {
+  let postmanParser: PostmanParser
+  let oasParser: OpenApiParser
+  let testSuite: TestSuite
+
+  const postmanJson = '__tests__/fixtures/crm-multi-responses.postman.json'
+  const oasYml = '__tests__/fixtures/crm-multi-responses.yml'
+
+  beforeEach(async () => {
+    oasParser = new OpenApiParser()
+    await oasParser.convert({ inputFile: oasYml })
+
+    const postmanObj = JSON.parse(fs.readFileSync(postmanJson).toString())
+    postmanParser = new PostmanParser({
+      collection: new Collection(postmanObj),
+      oasParser: oasParser
+    })
+  })
+
+  it('should use a specific content-type when multiple are available', async () => {
+    const configResult = await getConfig(
+      '__tests__/fixtures/portman-variations.multiple-ct-specific.json'
+    )
+    if (Either.isLeft(configResult)) {
+      return PortmanError.render(configResult.left)
+    }
+    const config = configResult.right
+    testSuite = new TestSuite({ oasParser, postmanParser, config })
+    testSuite.variationWriter = new VariationWriter({
+      testSuite: testSuite,
+      variationFolderName: 'Variation Tests'
+    })
+    testSuite.generateVariationTests()
+    expect(omitKeys(testSuite.collection.toJSON(), ['id', '_postman_id'])).toMatchSnapshot()
+  })
+
+  it('should handle wildcard content-type and generate multiple variations', async () => {
+    const configResult = await getConfig(
+      '__tests__/fixtures/portman-variations.multiple-ct-wildcard.json'
+    )
+    if (Either.isLeft(configResult)) {
+      return PortmanError.render(configResult.left)
+    }
+    const config = configResult.right
+    testSuite = new TestSuite({ oasParser, postmanParser, config })
+    testSuite.variationWriter = new VariationWriter({
+      testSuite: testSuite,
+      variationFolderName: 'Variation Tests'
+    })
     testSuite.generateVariationTests()
     expect(omitKeys(testSuite.collection.toJSON(), ['id', '_postman_id'])).toMatchSnapshot()
   })
